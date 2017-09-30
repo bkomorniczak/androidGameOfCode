65b469ebb2c1487f3d3567c547d37715a6571a4d
bkomorniczak <b.komorniczak@gmail.com>
2017-09-30 10:58:20 +0200

    first commit

---
+++ app/src/main/java/com/example/basia/gameofcode/characterActivity.java
@@ -0,0 +1,13 @@
+package com.example.basia.gameofcode;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class characterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_charater);
+    }
+}
